01ffb32fc645f18ea7a46b9878d808dda050526b
Lijie Huang <huanglijie@outlook.com>
2020-07-23 17:13:06 +0800

    milestone

--- mp_kafkaflow.py
+++ mp_kafkaflow.py
@@ -11,7 +11,6 @@ import subprocess
 import multiprocessing
 from configparser import ConfigParser
 
-
 import oss2
 from kafka import KafkaConsumer, KafkaProducer
 
@@ -24,16 +23,16 @@ class KafkaReceiver(multiprocessing.Process):
         multiprocessing.Process.__init__(self)
 
         self.consumer = KafkaConsumer(
-            envs['KAFKA_REC_TOPIC'],
-            group_id=envs['KAFKA_REC_GRP'],
+            envs['rec_topic'],
+            group_id = envs['rec_grp'],
             # enable_auto_commit=True,
             # auto_commit_interval_ms=2,
-            sasl_mechanism=envs['KAFKA_SASL_MECH'],
-            security_protocol=envs['KAFKA_SECURITY_PROTO'],
-            sasl_plain_username=envs['KAFKA_USER'],
-            sasl_plain_password=envs['KAFKA_PWD'],
-            bootstrap_servers = [envs['KAFKA_BOOTSTRAP_SERVERS']],
-            auto_offset_reset=envs['KAFKA_AUTO_OFFSET_RST'],
+            sasl_mechanism = envs['sasl_mechanism'],
+            security_protocol = envs['security_protocol'],
+            sasl_plain_username = envs['user'],
+            sasl_plain_password = envs['pwd'],
+            bootstrap_servers = [envs['bootstrap_servers']],
+            auto_offset_reset = envs['auto_offset_rst'],
         )
 
         self.queue = queue
@@ -42,21 +41,9 @@ class KafkaReceiver(multiprocessing.Process):
         """Receive message and put it in the queue."""
         # read message
         for msg in self.consumer:
-            #line = msg.value.decode('utf-8')
-            #line = line.strip()
+            line = msg.value.decode('utf-8').strip()
             #print(line)
-            self.queue.put(msg)
-
-
-def get_envs(env_file='.env'):
-    """Get env variables."""
-    with open(env_file, 'r') as f:
-        env_content = f.readlines()
-
-    env_content = [line.strip().split('=') for line in env_content
-                    if '=' in line]
-    envs = dict(env_content)
-    return envs
+            self.queue.put(line)
 
 def get_report_gallery(config_file='./report_gallery.config'):
     """Read report gallery config file."""
@@ -68,22 +55,32 @@ def get_report_gallery(config_file='./report_gallery.config'):
 def conn2aliyun(envs):
     """Connect to aliyun."""
     # aliyun oss auth
-    auth = oss2.Auth(envs['ACCESS_KEY_ID'], envs['ACCESS_KEY_SECRET'])
+    auth = oss2.Auth(envs['access_id'], envs['access_secret'])
     # get oss bucket
     bucket = oss2.Bucket(
         auth,
-        envs['OSS_ENDPOINT_NAME'],
-        envs['OSS_BUCKET_NAME'],
+        envs['oss_endpoint_name'],
+        envs['oss_bucket_name'],
     )
 
     return bucket
 
-def generate_report(user_id, report_type, data_dict=None,
+def generate_report(user_id, report_type, out_queue, data_dict=None,
                     bucket=None, base_url=None):
     """Workflow for generating report."""
+    # init return message
+    uploaded_msg = {
+        'user_id': user_id,
+        'report_type': report_type,
+        'status': 'ok',
+    }
+
     report_gallery = get_report_gallery()
 
     if report_type not in report_gallery:
+        uploaded_msg['status'] = 'error'
+        uploaded_msg['detail'] = 'Not find report type.'
+        out_queue.put(json.dumps(uploaded_msg))
         print('Error! Not find report type named %s'%(report_type))
         return None
 
@@ -119,11 +116,21 @@ def generate_report(user_id, report_type, data_dict=None,
         ipynb_file,
         '--output ' + html_file,
     ]
-    subprocess.run(' '.join(nbconvert_cmd), shell=True,
-                   env=dict(os.environ, USERID=user_id))
+    ret = subprocess.run(
+        ' '.join(nbconvert_cmd),
+        shell = True,
+        env = dict(os.environ, USERID=user_id),
+        stdout = subprocess.PIPE,
+        stderr = subprocess.PIPE,
+        encoding = 'utf-8',
+    )
     # check nbconvert output status
-    if not os.path.exists(html_file):
-        print('Error in nbconvert stage!')
+    if not ret.returncode==0:
+        uploaded_msg['status'] = 'error'
+        uploaded_msg['args'] = ret.args
+        uploaded_msg['stderr'] = ret.stderr
+        out_queue.put(json.dumps(uploaded_msg))
+        #print('Error in nbconvert stage!')
         return None
 
     # convert html file to standard format
@@ -152,50 +159,54 @@ def generate_report(user_id, report_type, data_dict=None,
         foreword_param,
         article_summary_param,
     ]
-    subprocess.run(' '.join(trans2std_cmd), shell=True)
+    ret = subprocess.run(' '.join(trans2std_cmd), shell=True)
     # check trans2std output status
-    if not os.path.exists(std_html_file):
-        print('Error in trans2std stage!')
+    if not ret.returncode==0:
+        uploaded_msg['status'] = 'error'
+        uploaded_msg['args'] = ret.args
+        uploaded_msg['stderr'] = ret.stderr
+        out_queue.put(json.dumps(uploaded_msg))
+        #print('Error in trans2std stage!')
         return None
 
     # convert html to pdf
-    pdf_file = os.path.join(pdf_dir, '%s_report.pdf'%(user_id))
+    ts = datetime.datetime.strftime(
+        datetime.datetime.now(),
+        '%Y%m%d%H%M%S',
+    )
+    pdf_filename = 'report_%s_%s.pdf'%(user_id, ts)
+    pdf_file = os.path.join(pdf_dir, pdf_filename)
     weasyprint_cmd = ['weasyprint', std_html_file, pdf_file]
-    subprocess.run(' '.join(weasyprint_cmd), shell=True)
+    ret = subprocess.run(' '.join(weasyprint_cmd), shell=True)
     # check weasyprint output status
-    if not os.path.exists(pdf_file):
-        print('Error in weasyprint stage!')
+    if not ret.returncode==0:
+        uploaded_msg['status'] = 'error'
+        uploaded_msg['args'] = ret.args
+        uploaded_msg['stderr'] = ret.stderr
+        out_queue.put(json.dumps(uploaded_msg))
+        #print('Error in weasyprint stage!')
         return None
 
     # clean
     if isinstance(json_file, str):
-        ts = datetime.datetime.strftime(
-            datetime.datetime.now(),
-            '%Y%m%d%H%M%S',
-        )
         targ_file = os.path.join(data_dir, '%s_%s.json'%(user_id, ts))
         shutil.move(json_file, targ_file)
     os.remove(html_file)
     os.remove(std_html_file)
 
-    remote_file = os.path.join(report_cfg['oss_dir'], '%s_report.pdf'%(user_id))
+    remote_file = os.path.join(report_cfg['oss_dir'], pdf_filename)
     
     # upload file
     remote_url = upload_file(bucket, base_url, pdf_file, remote_file)
-    uploaded_msg = {
-        'user_id': user_id,
-        urls: {report_type: remote_url},
-    }
-    print(uploaded_msg)
-    
-    # if uploaded_msg is not empty, send message
-    if uploaded_msg:
-        # XXX: add user id to uploaded_msg
-        # TODO: kafka_producer can be an instance of new class which
-        # wraps send_topic var
-        #kafka_producer.send(envs['KAFKA_SEND_TOPIC'], uploaded_msg)
-        pass
-
+    if remote_url:
+        uploaded_msg['status'] = 'ok'
+        uploaded_msg['urls'] = {report_type: remote_url}
+        out_queue.put(json.dumps(uploaded_msg))
+    else:
+        uploaded_msg['status'] = 'error'
+        uploaded_msg['args'] = 'Uploads to oss.'
+        uploaded_msg['stderr'] = 'Falied to upload pdf file.'
+        out_queue.put(json.dumps(uploaded_msg))
 
 def upload_file(bucket, base_url, src_file, remote_file):
     """Upload files to aliyun oss.
@@ -217,77 +228,92 @@ def upload_file(bucket, base_url, src_file, remote_file):
         print('%s error while uploading file %s'%(rsp.status, src_file))
         return None
 
-def delete_files(bucket, oss_list):
-    for oss_file in oss_list:
-        bucket.delete_object(oss_file)
-
 def queue_writer(q):
-    for i in range(20):
+    """For test."""
+    for i in range(10):
         time.sleep(random.random() * 10)
-        message = f'Message {i}'
-        q.put(message)
+        flag = random.randint(1, 51)
+        if flag>28:
+            msg = {
+                'reportType': 'mathDiagnosisK8_v1',
+                'data': '',
+            }
+        else:
+            msg = {
+                'reportType': 'mathDiagnosisK8_v1',
+                'data': {
+                    'id': '00'+str(i),
+                    'var1': 1,
+                    'var2': 2,
+                },
+            }
+        q.put(json.dumps(msg))
 
 
 if __name__ == '__main__':
-    # config
-    worker_num = 2
-
-    # get access key
-    envs = get_envs()
+    # read configs
+    envs = ConfigParser()
+    envs.read('./env.config')
 
     # create data queue for multiprocessing
     data_manager = multiprocessing.Manager()
-    queue = data_manager.Queue(100)
+    in_queue = data_manager.Queue(int(envs['general']['in_queue_size']))
+    out_queue = data_manager.Queue(int(envs['general']['out_queue_size']))
 
-
-    #-- initialize kafka msg receiver process
-    kafka_receiver = KafkaReceiver(envs, queue)
+    kafka_sender = KafkaProducer(
+        sasl_mechanism = envs['kafka']['sasl_mechanism'],
+        security_protocol = envs['kafka']['security_protocol'],
+        sasl_plain_username = envs['kafka']['user'],
+        sasl_plain_password = envs['kafka']['pwd'],
+        bootstrap_servers = [envs['kafka']['bootstrap_servers']],
+        value_serializer = lambda v: json.dumps(v).encode('utf-8'),
+        retries = 5,
+    )
+    #-- initialize kafka message receiver process
+    kafka_receiver = KafkaReceiver(envs['kafka'], in_queue)
     kafka_receiver.start()
 
-    # for test
-    # Create a writer
-    #multiprocessing.Process(target=queue_writer, args=(queue,)).start()
+    #XXX for test: Create a message writer
+    #multiprocessing.Process(target=queue_writer, args=(in_queue,)).start()
 
-
-    #-- process data
-    # initialize kafka producer
-    producer = KafkaProducer(
-        sasl_mechanism=envs['KAFKA_SASL_MECH'],
-        security_protocol=envs['KAFKA_SECURITY_PROTO'],
-        sasl_plain_username=envs['KAFKA_USER'],
-        sasl_plain_password=envs['KAFKA_PWD'],
-        bootstrap_servers = [envs['KAFKA_BOOTSTRAP_SERVERS']],
-        value_serializer = lambda v: json.dumps(v).encode('utf-8'),
-        retries=5,
-    )
-
-    # connect aliyun
-    bucket = conn2aliyun(envs)
-    base_url = '.'.join([envs['OSS_BUCKET_NAME'], envs['OSS_ENDPOINT_NAME']])
+    # connect to aliyun oss
+    bucket = conn2aliyun(envs['aliyun'])
+    base_url = '.'.join([
+        envs['aliyun']['oss_bucket_name'],
+        envs['aliyun']['oss_endpoint_name'],
+    ])
 
     # Create multiprocessing pool to process data
-    pool = multiprocessing.Pool(worker_num)
+    pool = multiprocessing.Pool(int(envs['general']['mp_worker_num']))
+
+    # logging
+    log_file = os.path.join(os.path.curdir, 'runinfo.log')
+    log_handler = open(log_file, 'a')
 
     # generate report
-    c = 1
     while True:
-        if not queue.empty():
-            raw_msg = queue.get()
-            msg = json.loads(raw_msg.value.decode('utf-8'))
+        if not in_queue.empty():
+            raw_msg = in_queue.get()
+            msg = json.loads(raw_msg)
             print(msg)
-            # XXX: get report type and user data from kafka message
-            report_type = msg['reportType']
+            # check the data validation
+            if not msg['data']:
+                ts = datetime.datetime.strftime(
+                    datetime.datetime.now(),
+                    '%Y%m%d-%H%M%S',
+                )
+                log_handler.write('[%s] No data found in %s\n'%(ts, str(msg)))
+                log_handler.flush()
+                #print('Not find data in message.')
+                continue
             data_dict = eval(msg['data'])
-            #data_dict = {
-            #    'user_id': 's00'+str(c),
-            #    'var1': 1,
-            #    'var2': 2,
-            #}
+            #data_dict = msg['data']
             pool.apply_async(
                 generate_report,
                 (
                     data_dict['id'],
-                    report_type,
+                    msg['reportType'],
+                    out_queue,
                 ),
                 {
                     'data_dict': data_dict,
@@ -295,22 +321,32 @@ if __name__ == '__main__':
                     'base_url': base_url,
                 },
             )
-            c += 1
+        elif not out_queue.empty():
+            msg = out_queue.get()
+            msg = json.loads(msg)
+            if msg['status']=='ok':
+                try:
+                    future = kafka_sender.send(envs['kafka']['send_topic'], msg)
+                    future.get(timeout=10)
+                except Exception as e:
+                    #print('Error!')
+                    #print(msg)
+                    #print(e)
+                    ts = datetime.datetime.strftime(
+                        datetime.datetime.now(),
+                        '%Y%m%d-%H%M%S',
+                    )
+                    log_handler.write('[%s] %s'%(ts, e)+'\n')
+                    log_handler.flush()
+            else:
+                ts = datetime.datetime.strftime(
+                    datetime.datetime.now(),
+                    '%Y%m%d-%H%M%S',
+                )
+                log_handler.write('[%s] %s'%(ts, str(msg))+'\n')
+                log_handler.flush()
+                #print(msg)
         else:
             time.sleep(0.01)
 
-
-    # remove file
-    #oss_list = [upload_files[k][1] for k in upload_files]
-    #delete_files(bucket, oss_list)
-    
-    #-- list files
-    #for obj in oss2.ObjectIterator(bucket, delimiter='/'):
-    #    # list dir
-    #    if obj.is_prefix():
-    #        print('-'*10)
-    #        print(obj.key)
-    #    # list files
-    #    else:
-    #        print(obj.key)
 
---
+++ sugar.py
@@ -0,0 +1,80 @@
+# vi: set ft=python sts=4 ts=4 sw=4 et:
+
+import os
+from itertools import islice
+from configparser import ConfigParser
+
+import oss2
+
+
+def conn2aliyun(envs):
+    """Connect to aliyun."""
+    # aliyun oss auth
+    auth = oss2.Auth(envs['access_id'], envs['access_secret'])
+    # get oss bucket
+    bucket = oss2.Bucket(
+        auth,
+        envs['oss_endpoint_name'],
+        envs['oss_bucket_name'],
+    )
+
+    return bucket
+
+def upload_file(bucket, base_url, src_file, remote_file):
+    """Upload files to aliyun oss.
+    Arguments:
+        bucket: oss bucket instance.
+        base_url: url of oss bucket.
+    """
+    # kafka message of upload files successfully
+    uploaded_msg = {}
+    rsp = bucket.put_object_from_file(
+        remote_file,
+        src_file,
+    )
+    # if upload successfully
+    # XXX: should check file content using etag field (md5)
+    if rsp.status==200:
+        return 'https://'+base_url+'/'+remote_file
+    else:
+        print('%s error while uploading file %s'%(rsp.status, src_file))
+        return None
+
+def delete_files(bucket, oss_list):
+    for oss_file in oss_list:
+        bucket.delete_object(oss_file)
+
+
+if __name__ == '__main__':
+    # read configs
+    envs = ConfigParser()
+    envs.read('./env.config')
+
+    # connect to aliyun oss
+    bucket = conn2aliyun(envs['aliyun'])
+    base_url = '.'.join([
+        envs['aliyun']['oss_bucket_name'],
+        envs['aliyun']['oss_endpoint_name'],
+    ])
+
+    # remove file
+    #oss_list = [upload_files[k][1] for k in upload_files]
+    #delete_files(bucket, oss_list)
+    
+    # remove selected files
+    sel_files = []
+    for obj in oss2.ObjectIterator(bucket):
+        # list dir
+        if obj.is_prefix():
+            print('-'*10)
+            print(obj.key)
+        # list files
+        else:
+            print(obj.key)
+            if 'mathDiagnosisK8_v1/' in obj.key:
+                sel_files.append(obj.key)
+
+    print(sel_files)
+    for item in sel_files:
+        bucket.delete_object(item)
+
