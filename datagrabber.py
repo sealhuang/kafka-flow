360c218a926ea12a08d59cdf842ee3fa23d73b6c
Lijie Huang <huanglijie@outlook.com>
2021-04-02 17:57:29 +0800

    update

--- datagrabber.py
+++ datagrabber.py
@@ -3,13 +3,13 @@
 import os
 import bson
 import json
-#import datetime
 import time
+from datetime import datetime
 import logging
 from configparser import ConfigParser
+from urllib.parse import quote_plus
 import pymongo
 from pymongo import UpdateOne, ReplaceOne
-from urllib.parse import quote_plus
 
 from loghandler import TimedRotatingCompressedFileHandler
 
@@ -41,7 +41,7 @@ def get_logger(log_level=logging.DEBUG):
 
     # set json format
     formatter = logging.Formatter(
-        '{"@timestamp":"%(asctime)s.%(msecs)03dZ","severity":"%(levelname)s","service":"jupyter-reporter",%(message)s}',
+        '{"@timestamp":"%(asctime)s.%(msecs)03dZ","severity":"%(levelname)s","service":"datagrabber",%(message)s}',
         datefmt='%Y-%m-%dT%H:%M:%S',
     )
     formatter.converter = time.gmtime
@@ -60,7 +60,7 @@ def conn2db(dbconfig):
         user = dbconfig.get('user')
         pwd = dbconfig.get('pwd')
 
-    user_info = ':'.join([item for item in [user, pwd] if item])
+    user_info = ':'.join([quote_plus(item) for item in [user, pwd] if item])
     if user_info:
         user_info = user_info + '@'
     url = 'mongodb://%s%s' % (user_info, dbconfig.get('url'))
@@ -76,41 +76,115 @@ def conn2db(dbconfig):
         print(err)
         return 'err', dbclient
 
+def apply_changes(change, datapool):
+    """Apply data changes to datapool."""
+    # config collections in datapool DB
+    uasge_stats_col = datapool['examUsageStats']
+
+    ns_db = change['ns']['db']
+    ns_col = change['ns']['coll']
+    op_type = change['operationType']
+
+    # apply changes in assessToken collection -- insert new token
+    if ns_col=='assessToken' and op_type=='insert':
+        item = {
+            'certificate_type': 'token',
+            'token': change['fullDocument']['code'],
+            'certificate_id': change['fullDocument']['_id'],
+            'usage_count': 0,
+            # TODO: add agent
+            #'agent': change['fullDocument']['xxx'],
+            'available_exam_type': change['fullDocument']['targetType'],
+        }
+        item['available_exam'] = [
+            ele['title'] for ele in change['fullDocument']['targets']
+        ]
+        item['creator'] = change['fullDocument']['creator']['name']
+        item['create_time'] = datetime.fromtimestamp(
+            change['fullDocument']['createTime']/1000
+        )
+        
+        # TODO: insert into collection
+
+    # changes in assessToken collection -- update token info
+    elif ns_col=='assessToken' and op_type=='update':
+        pass
+
+
+
+
+
+
+
 
 if __name__ == '__main__':
     # read configs
     envs = ConfigParser()
     envs.read(os.path.join(ROOT_DIR, 'env.config'))
+    # load db config
+    dbconfig = envs['mongodb']
 
     # initialize logger
     json_logger = get_logger()
 
-    # conn2db
-    dbconfig = envs['mongodb']
-    dbstatus, dbclient = conn2db(dbconfig)
-    # if no connection to db, close the programe
-    if dbstatus=='err':
-        exit()
+    # keep the db connection alive
+    dbstatus = 'err'
+    resume_token = None
+    while dbstatus=='err':
+        print('Connect to db ...')
+        dbstatus, dbclient = conn2db(dbconfig)
 
-    # watch data changes in db, and focus the specified collections
-    watched_db = dbclient[dbconfig.get('watch_db')]
-    pipeline = [
-        {'$match': {
-            'ns.coll': {
-                '$in': [
-                    'assessToken',
-                    'whitelistItem',
-                    'examAticket',
-                    'echainAticket',
-                ]
-            }
-        }},
-    ]
-    try:
-        with watched_db.watch() as stream:
-            for line in watched_db.watch(pipeline=pipeline):
-                print(line)
-    except pymongo.errors.PyMongoError as err:
-        print(err)
+        if dbstatus=='ok':
+            
+            json_logger.info('"rest":"Connect to db successfully"')
+
+            # watch data changes in db, and focus the specified collections
+            watched_db = dbclient[dbconfig.get('watch_db')]
+            datapool = dbclient[dbconfig.get('pool_db')]
+
+            pipeline = [
+                {'$match': {
+                    'ns.coll': {
+                        '$in': [
+                            'assessToken',
+                            'whitelistItem',
+                            'examAticket',
+                            'echainAticket',
+                        ]
+                    }
+                }},
+            ]
+            try:
+                with watched_db.watch(pipeline=pipeline,
+                        resume_after=resume_token) as stream:
+
+                    json_logger.info('"rest":"Start to watch data changes"')
+
+                    while stream.alive:
+                        change = stream.try_next()
+                        # get resume token, the resume token may be updated
+                        # even when no changes are returned
+                        resume_token = stream.resume_token
+
+                        # if get changes
+                        if change is not None:
+                            print(change)
+                            apply_changes(change, datapool)
+                            continue
+
+                        # if there are no recent changes, sleep for a while
+                        time.sleep(5)
+
+            except pymongo.errors.PyMongoError as err:
+                print(err)
+                json_logger.error(
+                    '"rest":"Error while get changes - %s"' % (str(err)),
+                    exc_info=True,
+                )
+                dbstatus = 'err'
+            else:
+                print("The ChangeStreams' cursor is broken.")
+                json_logger.error('"rest":"The ChangeStreams cursor is broken"')
+                dbstatus = 'err'
 
 
