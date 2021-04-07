54f662e9eb932cc717700085818e63c68eeaef32
Lijie Huang <huanglijie@outlook.com>
2021-04-07 18:17:05 +0800

    add exam usage data pool - token mode

--- datagrabber.py
+++ datagrabber.py
@@ -9,7 +9,7 @@ import logging
 from configparser import ConfigParser
 from urllib.parse import quote_plus
 import pymongo
-from pymongo import UpdateOne, ReplaceOne
+#from pymongo import UpdateOne, ReplaceOne
 
 from loghandler import TimedRotatingCompressedFileHandler
 
@@ -76,10 +76,14 @@ def conn2db(dbconfig):
         print(err)
         return 'err', dbclient
 
-def apply_changes(change, datapool):
+def apply_changes(change, dbclient):
     """Apply data changes to datapool."""
     # config collections in datapool DB
-    uasge_stats_col = datapool['examUsageStats']
+    datapool = dbclient[dbconfig.get('pool_db')]
+    usage_stats_col = datapool['examUsageStats']
+    # auxiliarytoken collection is used for search assess-token ID based on
+    # assess code and the used exam/exam-chain ID.
+    auxiliarytoken_col = dbclient['examstation']['auxiliarytoken']
 
     ns_db = change['ns']['db']
     ns_col = change['ns']['coll']
@@ -90,34 +94,244 @@ def apply_changes(change, datapool):
         item = {
             'certificate_type': 'token',
             'token': change['fullDocument']['code'],
-            'certificate_id': change['fullDocument']['_id'],
+            'certificate_id': str(change['fullDocument']['_id']),
             'usage_count': 0,
-            # TODO: add agent
-            #'agent': change['fullDocument']['xxx'],
             'available_exam_type': change['fullDocument']['targetType'],
         }
-        # XXX: to be confirmed
-        item['available_exam_id'] = [
-            ele['_id'] for ele in change['fullDocument']['targets']
-        ]
-        item['available_exam_name'] = [
+        item['available_exam'] = [
             ele['title'] for ele in change['fullDocument']['targets']
         ]
         item['creator'] = change['fullDocument']['creator']['name']
         item['create_time'] = datetime.fromtimestamp(
             change['fullDocument']['createTime']/1000
         )
+        # TODO: add taker info
+        if 'taker' in change['fullDocument']:
+            if 'name' in change['fullDocument']['taker']:
+                item['taker'] = change['fullDocument']['taker']['name']
+                item['take_time'] = datetime.fromtimestamp(
+                    change['fullDocument']['createTime']/1000
+                )
+        # TODO: add agent info
+        #'agent': change['fullDocument']['xxx'],
         
-        # TODO: insert into collection
+        # XXX: insert into collection
+        usage_stats_col.insert_one(item)
 
-    # changes in assessToken collection -- update token info
+    # changes in assessToken collection -- update taker info of token
     elif ns_col=='assessToken' and op_type=='update':
-        certificate_id = change['documentkey']['_id']
+        if 'taker' in change['updateDescription']['updatedFields']:
+            certificate_id = str(change['documentkey']['_id'])
+            taker_info = change['updateDescription']['updatedFields']['taker']
+            taker_name = taker_info['name']
+            take_time = datetime.fromtimestamp(
+                change['updateDescription']['updatedFields']['lUTime']/1000
+            )
+            # XXX: update db
+            usage_stats_col.update_one(
+                    {'certificate_id': certificate_id},
+                    {'$set': {'taker': taker_name, 'take_time': take_time}},
+            )
 
+    # handle event of exchanging eChain assess ticket
+    elif ns_col=='echainAticket' and op_type=='insert':
+        raw_doc = change['fullDocument']
+        
+        # token mode
+        token_recorded = False
+        if 'assessToken' in raw_doc:
+            # find token id
+            assess_token = raw_doc['assessToken']
+            target_type = 'ExamChain'
+            target_id = str(raw_doc['echain']['_id'])
+            res = auxiliarytoken_col.find_one({
+                'code': assess_token,
+                'targetType': target_type,
+                'targetId': target_id,
+            })
+            if isinstance(res, dict):
+                certificate_id = res['tokenId']
+                # assert the existence of the token in usageStats collection
+                token_info = usage_stats_col.find_one({
+                    'certificate_id': certificate_id
+                })
+                if isinstance(token_info, dict):
+                    token_recorded = True
+                    #certificate_type = 'token'
 
+        # TODO: add whitelist mode
+        else:
+            pass
 
+        if token_recorded:
+            upfields = {
+                'ticket_id': str(raw_doc['_id']),
+                'user_id': raw_doc['owner'],
+                'used_exam': raw_doc['echain']['title'],
+            }
+            if token_info['usage_count']==0:
+                upfields['first_use_time'] = datetime.fromtimestamp(
+                    raw_doc['lUTime']/1000
+                )
+            upfields['last_use_time'] = datetime.fromtimestamp(
+                raw_doc['lUTime']/1000
+            )
+            upfields['usage_count'] = token_info['usage_count'] + 1
+            # update user info
+            if 'province' in raw_doc:
+                upfields['province'] = raw_doc['province']
+            if 'city' in raw_doc:
+                upfields['city'] = raw_doc['city']
+            if 'county' in raw_doc:
+                upfields['region'] = raw_doc['county']
+            if 'studyingStatus' in raw_doc:
+                if 'school' in raw_doc['studyingStatus']:
+                    upfields['school'] = raw_doc['studyingStatus']['school']
+                if 'grade' in raw_doc['studyingStatus']:
+                    upfields['grade'] = raw_doc['studyingStatus']['grade']
+                if 'executiveClass' in raw_doc['studyingStatus']:
+                    upfields['class'] = raw_doc['studyingStatus']['executiveClass']
 
+            # XXX: update db
+            usage_stats_col.update_one(
+                    {'certificate_id': certificate_id},
+                    {'$set': upfields},
+            )
+                
+    # handle event of modifying user info
+    elif ns_col=='echainAticket' and op_type=='update':
+        to_be_update = False
+        for k in ['province', 'city', 'county', 'studyingStatus']:
+            if k in change['updateDescription']['updatedFields']:
+                to_be_update = True
+        
+        if to_be_update:
+            # assert the existence of the ticket in usageStats collection
+            ticket_id = str(change['documentKey']['_id'])
+            res = usage_stats_col.find_one({'ticket_id': ticket_id})
+            if isinstance(res, dict):
+                # update user info
+                upfields = {}
+                raw_doc = change['updateDescription']['updatedFields']
+                if 'province' in raw_doc:
+                    upfields['province'] = raw_doc['province']
+                if 'city' in raw_doc:
+                    upfields['city'] = raw_doc['city']
+                if 'county' in raw_doc:
+                    upfields['region'] = raw_doc['county']
+                if 'studyingStatus' in raw_doc:
+                    if 'school' in raw_doc['studyingStatus']:
+                        upfields['school'] = raw_doc['studyingStatus']['school']
+                    if 'grade' in raw_doc['studyingStatus']:
+                        upfields['grade'] = raw_doc['studyingStatus']['grade']
+                    if 'executiveClass' in raw_doc['studyingStatus']:
+                        upfields['class'] = raw_doc['studyingStatus']['executiveClass']
 
+                # XXX: update db
+                usage_stats_col.update_one(
+                    {'ticket_id': ticket_id},
+                    {'$set': upfields},
+                )
+
+    # handle event of exchanging exam assess ticket
+    elif ns_col=='examAticket' and op_type=='insert':
+        raw_doc = change['fullDocument']
+        
+        # token mode
+        token_recorded = False
+        if 'assessToken' in raw_doc:
+            # find token id
+            assess_token = raw_doc['assessToken']
+            target_type = 'Exam'
+            target_id = str(raw_doc['exam']['_id'])
+            res = auxiliarytoken_col.find_one({
+                'code': assess_token,
+                'targetType': target_type,
+                'targetId': target_id,
+            })
+            if isinstance(res, dict):
+                certificate_id = res['tokenId']
+                # assert the existence of the token in usageStats collection
+                token_info = usage_stats_col.find_one({
+                    'certificate_id': certificate_id
+                })
+                if isinstance(token_info, dict):
+                    token_recorded = True
+                    #certificate_type = 'token'
+
+        # TODO: add whitelist mode
+        elif 'assessToken' not in raw_doc and 'echainAticket' not in raw_doc:
+            pass
+
+        if token_recorded:
+            upfields = {
+                'ticket_id': str(raw_doc['_id']),
+                'user_id': raw_doc['owner'],
+                'used_exam': raw_doc['exam']['title'],
+            }
+            if token_info['usage_count']==0:
+                upfields['first_use_time'] = datetime.fromtimestamp(
+                    raw_doc['lUTime']/1000
+                )
+            upfields['last_use_time'] = datetime.fromtimestamp(
+                raw_doc['lUTime']/1000
+            )
+            upfields['usage_count'] = token_info['usage_count'] + 1
+            # update user info
+            if 'province' in raw_doc:
+                upfields['province'] = raw_doc['province']
+            if 'city' in raw_doc:
+                upfields['city'] = raw_doc['city']
+            if 'county' in raw_doc:
+                upfields['region'] = raw_doc['county']
+            if 'studyingStatus' in raw_doc:
+                if 'school' in raw_doc['studyingStatus']:
+                    upfields['school'] = raw_doc['studyingStatus']['school']
+                if 'grade' in raw_doc['studyingStatus']:
+                    upfields['grade'] = raw_doc['studyingStatus']['grade']
+                if 'executiveClass' in raw_doc['studyingStatus']:
+                    upfields['class'] = raw_doc['studyingStatus']['executiveClass']
+
+            # XXX: update db
+            usage_stats_col.update_one(
+                    {'certificate_id': certificate_id},
+                    {'$set': upfields},
+            )
+
+    # handle event of modifying user info
+    elif ns_col=='examAticket' and op_type=='update':
+        to_be_update = False
+        for k in ['province', 'city', 'county', 'studyingStatus']:
+            if k in change['updateDescription']['updatedFields']:
+                to_be_update = True
+        
+        if to_be_update:
+            # assert the existence of the ticket in usageStats collection
+            ticket_id = str(change['documentKey']['_id'])
+            res = usage_stats_col.find_one({'ticket_id': ticket_id})
+            if isinstance(res, dict):
+                # update user info
+                upfields = {}
+                raw_doc = change['updateDescription']['updatedFields']
+                if 'province' in raw_doc:
+                    upfields['province'] = raw_doc['province']
+                if 'city' in raw_doc:
+                    upfields['city'] = raw_doc['city']
+                if 'county' in raw_doc:
+                    upfields['region'] = raw_doc['county']
+                if 'studyingStatus' in raw_doc:
+                    if 'school' in raw_doc['studyingStatus']:
+                        upfields['school'] = raw_doc['studyingStatus']['school']
+                    if 'grade' in raw_doc['studyingStatus']:
+                        upfields['grade'] = raw_doc['studyingStatus']['grade']
+                    if 'executiveClass' in raw_doc['studyingStatus']:
+                        upfields['class'] = raw_doc['studyingStatus']['executiveClass']
+
+                # XXX: update db
+                usage_stats_col.update_one(
+                    {'ticket_id': ticket_id},
+                    {'$set': upfields},
+                )
 
 
 
@@ -145,7 +359,6 @@ if __name__ == '__main__':
 
             # watch data changes in db, and focus the specified collections
             watched_db = dbclient[dbconfig.get('watch_db')]
-            datapool = dbclient[dbconfig.get('pool_db')]
 
             pipeline = [
                 {'$match': {
@@ -174,7 +387,7 @@ if __name__ == '__main__':
                         # if get changes
                         if change is not None:
                             print(change)
-                            apply_changes(change, datapool)
+                            apply_changes(change, dbclient)
                             continue
 
                         # if there are no recent changes, sleep for a while
