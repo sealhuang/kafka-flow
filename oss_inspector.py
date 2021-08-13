b2581fe6cc9e3124d4c24978a3634e0713144f67
Lijie Huang <huanglijie@outlook.com>
2021-08-13 16:41:34 +0800

    update oss-inspector

--- oss_inspector.py
+++ oss_inspector.py
@@ -58,7 +58,7 @@ if __name__ == '__main__':
     ])
 
     # upload file
-    upload_file(bucket, base_url, 'test.pdf', 'test/test.pdf')
+    #upload_file(bucket, base_url, 'test.pdf', 'test/test.pdf')
 
     # remove file
     #oss_list = [upload_files[k][1] for k in upload_files]
@@ -76,8 +76,54 @@ if __name__ == '__main__':
     #        print(obj.key)
     #        if 'mathDiagnosisK8_v1/' in obj.key:
     #            sel_files.append(obj.key)
-
     #print(sel_files)
     #for item in sel_files:
     #    bucket.delete_object(item)
+
+    #-- remove redundant files
+    # get all files in the selected dir
+    all_files = []
+    sel_dir_name = 'personalStatusYixin_v1/'
+    for obj in oss2.ObjectIterator(bucket):
+        if obj.key.startswith(sel_dir_name):
+            all_files.append(obj.key)
+    print('Fetch %s files in DIR %s' % (len(all_files), sel_dir_name))
+    # remove dir name from file path
+    all_files = [item.replace(sel_dir_name, '') for item in all_files]
+
+    # find redundant files
+    sid_file_dict = {}
+    for fname in all_files:
+        fparts = fname.replace('.pdf', '').split('_')
+        sid = fparts[1]
+        if sid not in sid_file_dict:
+            sid_file_dict[sid] = []
+        sid_file_dict[sid].append(fname)
+    print('Find %s sids' % (len(sid_file_dict)))
+    ## XXX: for test
+    #test_key = list(sid_file_dict.keys())[0]
+    #sid_file_dict = {
+    #    test_key: sid_file_dict[test_key]
+    #}
+    #print(sid_file_dict)
+    del_files = []
+    for sid in sid_file_dict:
+        if len(sid_file_dict[sid])<2:
+            continue
+        file_datetime = [
+            int(item.replace('.pdf', '').split('_')[-1]) for item in sid_file_dict[sid]
+        ]
+        max_dt = file_datetime[0]
+        for dt in file_datetime:
+            if dt>=max_dt:
+                max_dt = dt
+        _del_list = [
+            item for item in sid_file_dict[sid] if str(max_dt)+'.pdf' not in item
+        ]
+        #print(_del_list)
+        del_files = del_files + _del_list
+    print('Find %s files to be deleted' % (len(del_files)))
+ 
+    for item in del_files:
+        bucket.delete_object(sel_dir_name+item)
 
